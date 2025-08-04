e9a9dd7f5a6ed8e0c41da611ad3866158116e06a
lopespkm <59057440+lopespkm@users.noreply.github.com>
2025-08-04 18:19:02 -0300

    front

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'api.ultrapanel.shop',
+        hostname: 'api.raspaeganha.net',
+        port: '',
         pathname: '/uploads/**',
       },
+      // Supabase CDN
       {
         protocol: 'https',
         hostname: '**.supabase.co',
+        port: '',
         pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ultrapanel.shop',
-        pathname: '/**', // permite qualquer imagem do domínio
-      },
-      {
-        protocol: 'https',
-        hostname: 'sucessogrupo.shop',
-        pathname: '/imagemsras/**', // ajuste conforme necessário
-      },
+      }
     ],
   },
 };
